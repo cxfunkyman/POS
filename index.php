2b20d2658ccec9c2c99a2c67cc5974482a8fd7a4
cxfunkyman <92549556+cxfunkyman@users.noreply.github.com>
2022-11-28 14:19:17 -0500

    corrections

--- index.php
+++ index.php
@@ -13,42 +13,31 @@ $controller = ucfirst($array[0]);
 $method = 'index';
 $parameter = '';
 
-if(!empty($array[1]))
-{
-    if($array[1] != '')
-    {
+if (!empty($array[1])) {
+    if ($array[1] != '') {
         $method = $array[1];
     }
 }
-if(!empty($array[2]))
-{
-    if($array[2] != '')
-    {
-    for ($i=2; $i < count($array); $i++) { 
+if (!empty($array[2])) {
+    if ($array[2] != '') {
+    for ($i=2; $i < count($array); $i++) {
             $parameter .= $array[$i] . ',';
         }
-        $parameter = trim($parameter, ',');        
+        $parameter = trim($parameter, ',');
     }
 }
 require_once 'config/app/Autoload.php';
 $dirController = 'controllers/' . $controller . '.php';
 
-if(file_exists($dirController))
-{
+if (file_exists($dirController)) {
     require_once $dirController;
     $controller = new $controller();
 
-    if(method_exists($controller, $method))
-    {
+    if (method_exists($controller, $method)) {
         $controller -> $method($parameter);
-    }
-    else 
-    {
+    } else {
         echo 'Method does not exist';
     }
-}
-else 
-{
+} else {
     echo 'Controller does not exist';
 }
-?>
